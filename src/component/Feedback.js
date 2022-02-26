3ecff145b7dbef6ad905bc1ec03b9ece5b128246
NatheenRamalingam <natheenrls@gmail.com>
2022-02-26 17:01:20 +0530

    changes

--- src/component/Feedback.js
+++ src/component/Feedback.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import {  db } from './firebase';
-import {toast} from 'react-toastify'
 
 function Feedback() {
     const [name,setName] = useState('');
@@ -8,11 +7,8 @@ function Feedback() {
     const[email,setEmail] = useState('');
     const[message,setMessage] = useState('');
 
-
-
     const handleSubmit = (e)=>{
-        e.preventDefault();     
-
+        e.preventDefault();  
         db.collection('feedback')
             .add({
                 name : name,
@@ -20,25 +16,19 @@ function Feedback() {
                 email : email,
                 message : message
             })
-            alert("Form submitted Succesfully");       
-        
-           
+            alert("Form submitted Succesfully");     
             setName('');
             setMobile('');
             setEmail('');
             setMessage('');
     }
 
-
-
-
-
   return (
     <div className='col-md-6 offset-3 mt-1'>
         <div className='card'>
             <div className='card-body'>
                 <h1 className='text-center'>Feedback Form</h1>
-                <form action="">
+                <form action="">                    
                     <label htmlFor="">Name</label>
                     <input type="text"  value={name} onChange={(e)=>{setName(e.target.value)}} className='form-control' />
                     <label htmlFor="">Mobile</label>
@@ -47,14 +37,10 @@ function Feedback() {
                     <input type="email"  value={email} onChange={(e)=>{setEmail(e.target.value)}} className='form-control' />
                     <label htmlFor="">Message</label>
                     <textarea name="message"  value={message} onChange={(e)=>{setMessage(e.target.value)}} id="" cols="30" rows="10" className='form-control'/>
-                    <button onClick={(e)=>{handleSubmit(e)}} className='btn btn-outline-success mt-3'>Submit</button>
-                    
+                    <button onClick={(e)=>{handleSubmit(e)}} className='btn btn-outline-success mt-3'>Submit</button>     
                 </form>
-
             </div>
-
         </div>
-
     </div>
   )
 }
